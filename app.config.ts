63151adb7c35b72e2055af3b08ee33e1635eab5e
ActuallyPanda56 <nietoc0595@gmail.com>
2024-11-12 21:07:26 -0500

    <fix> app config

--- app.config.ts
+++ app.config.ts
@@ -1,15 +1,15 @@
 import "dotenv/config";
 
-export default ({ config }) => ({
-  ...config,
-  extra: {
-    STRAVA_API: {
-      CLIENT_ID: process.env.STRAVA_CLIENT_ID || "139892",
-      CLIENT_SECRET:
-        process.env.STRAVA_CLIENT_SECRET ||
-        "08f79c7faf9429c48650525a8c0524f6b6ae5351",
-      REDIRECT_URI:
-        process.env.STRAVA_REDIRECT_URI || "exp://192.168.1.20:8081",
+export default {
+  expo: {
+    name: "strava-test",
+    slug: "strava-test",
+    extra: {
+      STRAVA_API: {
+        CLIENT_ID: process.env.STRAVA_CLIENT_ID,
+        CLIENT_SECRET: process.env.STRAVA_CLIENT_SECRET,
+        REDIRECT_URI: process.env.STRAVA_REDIRECT_URI,
+      },
     },
   },
-});
+};
